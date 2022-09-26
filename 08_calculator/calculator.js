af725fec7a00eef259c01e80610274ec06cbf09c
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-26 22:13:07 +0100

    update
    solve the empty array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,9 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-
+const sum = function([]) {
+return 
+sum.length -1;
 };
 
 const multiply = function(num1, num2) {
