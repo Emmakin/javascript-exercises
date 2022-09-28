1b8128c3e376d2046e15839c5dc82eb34bf8a383
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-28 22:49:55 +0100

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
 };
 
 // Do not edit below this line
